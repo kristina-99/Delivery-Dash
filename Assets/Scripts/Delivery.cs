7fd3354eda9d8e5610c9e5dc724ebda7329056d6
kristina-99 <kristina_atan@abv.bg>
2026-03-11 21:01:40 +0200

    * added logic for destroying packages after picking up

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool hasPackage;
+    [SerializeField] float delay = 1f;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Package"))
+        if (collision.CompareTag("Package") && !hasPackage)
         {
             Debug.Log("Picked up package");
             hasPackage = true;
+            Destroy(collision.gameObject, delay);
         }
 
         if (collision.CompareTag("Customer") && hasPackage)
